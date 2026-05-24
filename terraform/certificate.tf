20813cf1c132d709d64013bc4808422857aa4979
kabongo <henry@hoitcs.com>
2026-05-24 20:02:15 +1000

    added cert verification

--- terraform/certificate.tf
+++ terraform/certificate.tf
@@ -11,3 +11,29 @@ resource "aws_acm_certificate" "workflow-cert" {
   }
   
 }
+
+data "aws_route53_zone" "primary" {
+  name         = "wazobia.com.au."
+  private_zone = false
+}
+
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.workflow-cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      type   = dvo.resource_record_type
+      record = dvo.resource_record_value
+    }
+  }
+
+  zone_id = data.aws_route53_zone.primary.zone_id
+  name    = each.value.name
+  type    = each.value.type
+  records = [each.value.record]
+  ttl     = 60
+}
+
+resource "aws_acm_certificate_validation" "cert_validation" {
+  certificate_arn         = aws_acm_certificate.workflow-cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+}
