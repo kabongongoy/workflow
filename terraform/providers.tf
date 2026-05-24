4976823849df6476efd7adb362e1263a7cbabfe9
kabongo <henry@hoitcs.com>
2026-05-24 18:58:24 +1000

    added provider backend file to bucket

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,13 +1,21 @@
-provider "aws" {
-  alias  = "us_east_1"
-  region = "us-east-1"
-}
-
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~> 5.0" 
     }
   }
+  
+  # S3 Backend for state storage with DynamoDB locking
+  backend "s3" {
+    bucket         = "hoitcs-terraform-state"
+    key            = "workflow/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "hoitcs-terraform-locks"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region  = "us-east-1"
 }
