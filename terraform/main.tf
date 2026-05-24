e2fc010b5362b8619e7acf610bc3e6ab6fa3eeaa
kabongo <henry@hoitcs.com>
2026-05-24 19:15:24 +1000

    configre bucket for html

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,40 @@
 resource "aws_s3_bucket" "static-site-bucket" {
   bucket = var.bucket_name 
 }
+
+resource "aws_s3_bucket_public_access_block" "block_public_access" {
+  bucket = aws_s3_bucket.static-site-bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_website_configuration" "static_site" {
+  bucket = aws_s3_bucket.static-site-bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.static-site-bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = "*"
+        Action = "s3:GetObject"
+        Resource = "${aws_s3_bucket.static-site-bucket.arn}/*"
+      }
+    ]
+  })
+}
