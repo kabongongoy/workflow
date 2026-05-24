222560a37b83a5d744d8d4c26eb9e4e6f47fa78a
kabongo <henry@hoitcs.com>
2026-05-24 19:41:02 +1000

    remove permission for  bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,37 +5,37 @@ resource "aws_s3_bucket" "static-site-bucket" {
 resource "aws_s3_bucket_public_access_block" "block_public_access" {
   bucket = aws_s3_bucket.static-site-bucket.id
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
-resource "aws_s3_bucket_website_configuration" "static_site" {
-  bucket = aws_s3_bucket.static-site-bucket.id
+# resource "aws_s3_bucket_website_configuration" "static_site" {
+#   bucket = aws_s3_bucket.static-site-bucket.id
 
-  index_document {
-    suffix = "index.html"
-  }
+#   index_document {
+#     suffix = "index.html"
+#   }
 
-  error_document {
-    key = "error.html"
-  }
-}
+#   error_document {
+#     key = "error.html"
+#   }
+# }
 
-resource "aws_s3_bucket_policy" "bucket_policy" {
-  bucket = aws_s3_bucket.static-site-bucket.id
+# resource "aws_s3_bucket_policy" "bucket_policy" {
+#   bucket = aws_s3_bucket.static-site-bucket.id
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = "*"
-        Action = "s3:GetObject"
-        Resource = "${aws_s3_bucket.static-site-bucket.arn}/*"
-      }
-    ]
-  })
-  depends_on = [aws_s3_bucket_public_access_block.block_public_access]
-}
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Effect = "Allow"
+#         Principal = "*"
+#         Action = "s3:GetObject"
+#         Resource = "${aws_s3_bucket.static-site-bucket.arn}/*"
+#       }
+#     ]
+#   })
+#   depends_on = [aws_s3_bucket_public_access_block.block_public_access]
+# }
